b62fecc8e716b73fb7a8c4675b7813a008eee040
z8w33ss2 <123525733+z8w33ss2@users.noreply.github.com>
2024-06-09 20:55:18 -0600

    Versión del proyecto Zoologico practica de la semana 3, introducción a CSS

---
+++ src/main/java/com/practica2/Practica2Application.java
@@ -0,0 +1,13 @@
+package com.practica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2Application.class, args);
+	}
+
+}
